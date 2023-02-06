a14e4f660e6e4a723f835ad203ba8c406e266c4c
Emmanuel-7orres <99225096+Emmanuel-7orres@users.noreply.github.com>
2023-02-05 18:51:07 -0800

    Moved things from openlab
    -Trap detection
    -Writing information to files

--- scraper.py
+++ scraper.py
@@ -1,14 +1,16 @@
 import re
 from urllib.parse import urlparse, urldefrag
+
 from bs4 import BeautifulSoup
 
 def scraper(url, resp):
     links = extract_next_links(url, resp)
-    #print("Total Unique Links: ", Total_links)
+    for link in links:
+        if(is_valid(link)):
+            print(link)
     return [link for link in links if is_valid(link)]
 
 def extract_next_links(url, resp):
-    #use geturl
     # Implementation required.
     # url: the URL that was used to get the page
     # resp.url: the actual url of the page
@@ -18,18 +20,17 @@ def extract_next_links(url, resp):
     #         resp.raw_response.url: the url, again
     #         resp.raw_response.content: the content of the page!
     # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
-
     links = set()
+    if(resp.status != 200 and resp.error != None):
+        return []
+    try:
+        soup = BeautifulSoup(resp.raw_response.content, 'html.parser')
+    except AttributeError:
+        return []
     
-    soup = BeautifulSoup(resp.raw_response.content, 'html.parser')
-
     for link in soup.find_all('a'):
-        if is_valid(link.get('href')):
-            # try:
-            links.add(urldefrag(link.get('href'))[0])
-            # except AttributeError:
-            #     pass
-
+        print(urldefrag(link.get('href'))[0])
+        links.add(urldefrag(link.get('href'))[0])
     # for link in links:
     #     print(link)
     #Total_links += len(links)
@@ -40,15 +41,40 @@ def is_valid(url):
     # Decide whether to crawl this url or not. 
     # If you decide to crawl it, return True; otherwise return False.
     # There are already some conditions that return False.
-
     domains = [".ics.uci.edu", ".cs.uci.edu", ".informatics.uci.edu",".stat.uci.edu"]
+    traps = ["/events", "/event", "/calendar"]
     try:
         parsed = urlparse(url)
-        
+        visited = set()
+
+        with open("visited.txt", "a+") as Visit:
+            for line in Visit:
+                visited.add(line)
+            if url in visited:
+                return False
+
         for d in domains:
-            if d in str(parsed.netloc):   
+            if d in str(parsed.netloc):
+                for trap in traps:
+                    if trap in parsed.path.lower():
+                        return False
+
                 if parsed.scheme not in set(["http", "https"]):
                     return False
+                
+                if not re.match(
+                    r".*\.(css|js|bmp|gif|jpe?g|ico"
+                    + r"|png|tiff?|mid|mp2|mp3|mp4"
+                    + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
+                    + r"|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names"
+                    + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
+                    + r"|epub|dll|cnf|tgz|sha1"
+                    + r"|thmx|mso|arff|rtf|jar|csv"
+                    + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower()):
+                    with open("visited.txt", "a+") as Visit:
+                       Visit.write(url, "\n")
+                    
+
                 return not re.match(
                     r".*\.(css|js|bmp|gif|jpe?g|ico"
                     + r"|png|tiff?|mid|mp2|mp3|mp4"
